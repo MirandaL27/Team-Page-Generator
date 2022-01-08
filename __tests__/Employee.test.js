53ab5e79181ea7a35733f71b61ba512201e4f1d5
Cadenza <mirandalucsb@gmail.com>
2022-01-08 16:00:54 -0700

    added questions array to and tested employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,41 @@
 const Employee = require("../lib/Employee.js");
 
 test('creates an employee object', () =>{
-    const employee = new Employee('Jarod Prower', '1234', 'JPrower@aol.com');
-
+    const employee = new Employee('Jarod Prower', '1234', 'JPrower@aol.com', 'employee');
+    const typeStr = employee.type;
     expect(employee.name).toBe('Jarod Prower'); 
     expect(employee.id).toBe('1234');
     expect(employee.email).toBe('JPrower@aol.com');
+    expect(employee.type).toBe('employee');
+
+    expect(employee.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'name',
+                message: `What is the name of this ${typeStr}?`
+            })
+        ])
+    )
+    expect(employee.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'id',
+                message: `What is the id of this ${typeStr}?`
+            })
+        ])
+    )
+    expect(employee.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'email',
+                message: `What is the email of this ${typeStr}?`
+            })
+        ])
+    )
+    
 })
 
 test('gets employee name', () =>{
