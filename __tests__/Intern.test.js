374d0fbedd6b4a92b530af9a607207b1121d75a4
Cadenza <mirandalucsb@gmail.com>
2022-01-08 16:13:48 -0700

    added questions to intern and tested it.

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,12 +1,50 @@
 const Intern = require("../lib/Intern.js");
 
 test('creates an intern object', () =>{
-    const intern = new Intern('Jarod Prower', '1234', 'JPrower@aol.com','UCSB');
-
+    const intern = new Intern('Jarod Prower', '1234', 'JPrower@aol.com','UCSB','intern');
+    const typeStr = intern.type;
     expect(intern.name).toBe('Jarod Prower'); 
     expect(intern.id).toBe('1234');
     expect(intern.email).toBe('JPrower@aol.com');
     expect(intern.school).toBe('UCSB');
+    expect(intern.type).toBe('intern');
+
+    expect(intern.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'name',
+                message: `What is the name of this ${typeStr}?`
+            })
+        ])
+    )
+    expect(intern.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'id',
+                message: `What is the id of this ${typeStr}?`
+            })
+        ])
+    )
+    expect(intern.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'email',
+                message: `What is the email of this ${typeStr}?`
+            })
+        ])
+    )
+    expect(intern.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'school',
+                message: `What is the school of this ${typeStr}?`
+            })
+        ])
+    )
 })
 
 test('gets Intern School', () =>{
