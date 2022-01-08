c74e10b6624d57deb09ae5bef1a1dc9fb42349b4
Cadenza <mirandalucsb@gmail.com>
2022-01-08 16:08:56 -0700

    added questions to engineer and tested it.

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,51 @@
 const Engineer = require("../lib/Engineer.js");
 
 test('creates an engineer object', () =>{
-    const engineer = new Engineer('Jarod Prower', '1234', 'JPrower@aol.com','MirandaL27');
-
+    const engineer = new Engineer('Jarod Prower', '1234', 'JPrower@aol.com','MirandaL27','engineer');
+    const typeStr = engineer.type;
     expect(engineer.name).toBe('Jarod Prower'); 
     expect(engineer.id).toBe('1234');
     expect(engineer.email).toBe('JPrower@aol.com');
     expect(engineer.gitHub).toBe('MirandaL27');
+    expect(engineer.type).toBe('engineer');
+
+    expect(engineer.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'name',
+                message: `What is the name of this ${typeStr}?`
+            })
+        ])
+    )
+    expect(engineer.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'id',
+                message: `What is the id of this ${typeStr}?`
+            })
+        ])
+    )
+    expect(engineer.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'email',
+                message: `What is the email of this ${typeStr}?`
+            })
+        ])
+    )
+    expect(engineer.questions).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                type:'input',
+                name: 'gitHub',
+                message: `What is the github username of this ${typeStr}?`
+            })
+        ])
+    )
+    
 })
 
 test('gets engineer github', () =>{
