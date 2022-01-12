f4fc0000e79466dca207b2820444e87ea64bbe7c
Cadenza <mirandalucsb@gmail.com>
2022-01-12 12:21:32 -0700

    add html generator functions.

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,7 +2,34 @@ const Manager = require('../lib/Manager.js');
 const Engineer = require('../lib/Engineer.js');
 const Intern = require('../lib/Intern.js');
 
+const generateEmployeeHTML = data =>{
+    let outputStr = '';
+    data.foreach(data => {
+        outputStr += `<div class="${data.type}">
+        <p>Name: ${data.name}</p>
+        <p>Id: ${data.id}</p>
+        <p>Email: ${data.email}</p>
+        `
+
+        if(data.type === "manager"){
+            outputStr +=`<p>Office Number: ${data.officeNumber}</p>`
+        }
+        else if(data.type === "engineer"){
+            outputStr +=`<p>GitHub: https://github.com/${data.gitHub}</p>`
+        }
+        else{
+            outputStr += `<p>School: ${data.school}</p>`
+        }
+    })
+
+    return outputStr;
+}
+
 var generateHTML = (data) => {
+    console.log(data);
+    data.foreach(data => {
+
+    })
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -10,7 +37,7 @@ var generateHTML = (data) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>My Team</title>
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
     <!--header section-->
@@ -18,23 +45,8 @@ var generateHTML = (data) => {
             <h1>My Team</h1>
         </header>
     <main>
-        <div class="manager">
-            <p>name: Miranda Lane</p>
-            <p>id: 1234</p>
-            <p>email: m@aol.com</p>
-            <p>office number: 2345</p>
-        </div>
-        <div class="engineer">
-            <p>name: Poppy Dada</p>
-            <p>id: 3456</p>
-            <p>email: p@aol.com</p>
-            <p>github: dada24</p>
-        </div>
-        <div class="intern">
-            <p>name: Micheal Washington</p>
-            <p>id: 4567</p>
-            <p>email: w@aol.com</p>
-            <p>school: UCSB</p>
+        <div class="employee-container">
+            ${generateEmployeeHTML(data)}
         </div>
     </main>    
     </body>
